c59bd093971194519d7d28a3e0d6649b72d7cc87
Robbie <robbiethewadley@gmail.com>
2022-06-02 10:47:39 -0600

    debugging

--- selfdrive/car/volkswagen/carstate.py
+++ selfdrive/car/volkswagen/carstate.py
@@ -197,6 +197,7 @@ class CarState(CarStateBase):
     else:
       ret.gas = (cam_cp.vl["GAS_SENSOR"]['INTERCEPTOR_GAS'] + cam_cp.vl["GAS_SENSOR"]['INTERCEPTOR_GAS2']) / 2.
       ret.gasPressed = ret.gas > 468
+      print("GAS: %f" % ret.gas)
 
     ret.brake = pt_cp.vl["Bremse_5"]["Bremsdruck"] / 250.0  # FIXME: this is pressure in Bar, not sure what OP expects
     ret.brakePressed = bool(pt_cp.vl["Motor_2"]["Bremstestschalter"])
